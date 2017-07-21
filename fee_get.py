2e98deb957677097aabb39220c9ec09cd5d773bf
YangChenguang <ycg1024@users.noreply.github.com>
2017-07-21 16:58:35 +0800

    2017/07/21

--- fee_get.py
+++ fee_get.py
@@ -1,69 +1,185 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
+import time
 import requests
 import lxml.html
 from mailsend import *
 
 
-def main():
+def TEB_readfromjson(mode=0):
+    '''
+    >>> read information from json files
+    mode=0: RELEASE  
+    mode=1: DEBUG
+    '''
+    if mode == 0:
+        print('[INFO] try to Read TEB_config.json ...')
+        try:
+            with open("TEB_config.json", "r") as f1:
+                text = f1.read()
+            TEB_jsonData = json.loads(text)
+        except Exception:
+            print(
+                '[INFO] Read failed! Please check if the file [TEB_config.json] is existed...')
+            exit()
+    else:
+        print('[INFO] __DEBUG MODE__')
+        print('[INFO] try to Read TEB_config_test.json ...')
+        try:
+            with open("TEB_config_test.json", "r") as f1:
+                text = f1.read()
+            TEB_jsonData = json.loads(text)
+        except Exception:
+            print(
+                '[INFO] Read failed! Please check if the file [TEB_config_test.json] is existed...')
+            exit()
+    print('[INFO] Read succesful ...')
 
-    url = "http://202.120.163.129:88/"
+    return TEB_jsonData
 
-    fee_threshold = 1.0
 
-    print('[INFO] read TEB_config.json ...')
-    with open("TEB_config.json", "r") as f:
-        text = f.read()
-    json_data = json.loads(text)
-    print('[INFO] read succesful ...')
+def TEB_getFeeFromPage(url, headers, form_data):
+    '''
+    return (float)fee
+    '''
+    form_data_post = {
+        "__VIEWSTATE": "",
+        "__VIEWSTATEGENERATOR": "",
+        "drlouming": "",
+        "drceng": "",
+        "dr_ceng": "",
+        "drfangjian": "",
+        "radio": "usedR",
+        "ImageButton1.x": "",
+        "ImageButton1.y": ""
+    }
+    # TEBsess = requests.session()
+    with requests.Session() as TEBsess:
 
-    headers = {'User-Agent': json_data['user_agent']}
-
-    mailserver_username = json_data['mailserver_username']  # 发送提醒邮件的邮箱
-    mailserver_password = json_data['mailserver_password']  # 发送提醒邮件邮箱的密码
-    mailserver_smtp = json_data['mailserver_smtp']  # 发送邮箱的SMPT服务器地址
-
-    for room in json_data['clientroom']:
-        form_data = json_data['clientroom'][room]['form_data']
-        mailclient = json_data['clientroom'][room]['mailclient']
-
-        r = requests.post(url, headers=headers, data=form_data)
+        r = TEBsess.get(url, headers=headers, timeout=5)  # 第1次 post
         page = lxml.html.document_fromstring(r.text)
-        fee = float(page.xpath("//span[@class='number orange']/text()")[0])
+        VIEWSTATE = page.xpath("//input[@name='__VIEWSTATE']/@value")
+        VIEWSTATEGENERATOR = page.xpath(
+            "//input[@name='__VIEWSTATEGENERATOR']/@value")
+        form_data_post['__VIEWSTATE'] = VIEWSTATE
+        form_data_post['__VIEWSTATEGENERATOR'] = VIEWSTATEGENERATOR
+        form_data_post['drlouming'] = form_data['drlouming']
 
-        print(" 嘉定" + " 校区  " + str(room)[-4]
-              + " 号楼  " + str(room)[-3:] + " 房间 " + " 电费还剩：" + str(fee))
+        r = TEBsess.post(url, headers=headers,
+                         data=form_data_post, timeout=5)  # 第2次 post
+        page = lxml.html.document_fromstring(r.text)
+        VIEWSTATE = page.xpath("//input[@name='__VIEWSTATE']/@value")
+        VIEWSTATEGENERATOR = page.xpath(
+            "//input[@name='__VIEWSTATEGENERATOR']/@value")
+        form_data_post['__VIEWSTATE'] = VIEWSTATE
+        form_data_post['__VIEWSTATEGENERATOR'] = VIEWSTATEGENERATOR
+        form_data_post['drceng'] = form_data['drceng']
 
-        regular_mailclient = [to_addr for [to_addr, isRegular] in mailclient.items() if isRegular]
+        r = TEBsess.post(url, headers=headers,
+                         data=form_data_post, timeout=5)  # 第3次 post
+        page = lxml.html.document_fromstring(r.text)
+        VIEWSTATE = page.xpath("//input[@name='__VIEWSTATE']/@value")
+        VIEWSTATEGENERATOR = page.xpath(
+            "//input[@name='__VIEWSTATEGENERATOR']/@value")
+        form_data_post['__VIEWSTATE'] = VIEWSTATE
+        form_data_post['__VIEWSTATEGENERATOR'] = VIEWSTATEGENERATOR
+        form_data_post['dr_ceng'] = form_data['dr_ceng']
 
-        if  regular_mailclient:
-            print('[INFO] Send mail to regular receiver: ' + str(regular_mailclient))
+        r = TEBsess.post(url, headers=headers,
+                         data=form_data_post, timeout=5)  # 第4次 post
+        page = lxml.html.document_fromstring(r.text)
+        VIEWSTATE = page.xpath("//input[@name='__VIEWSTATE']/@value")
+        VIEWSTATEGENERATOR = page.xpath(
+            "//input[@name='__VIEWSTATEGENERATOR']/@value")
+        form_data_post['__VIEWSTATE'] = VIEWSTATE
+        form_data_post['__VIEWSTATEGENERATOR'] = VIEWSTATEGENERATOR
+        form_data_post['drfangjian'] = form_data['drfangjian']
+        form_data_post['radio'] = form_data['radio']
+        form_data_post['ImageButton1.x'] = form_data['ImageButton1.x']
+        form_data_post['ImageButton1.y'] = form_data['ImageButton1.y']
 
-            regularMailSend(mailserver_username=mailserver_username,
-                            mailserver_password=mailserver_password,
-                            mailserver_smtp=mailserver_smtp,
-                            mailclient=regular_mailclient,
-                            clientroom=room,
-                            fee=fee)
+        r = TEBsess.post(url, headers=headers,
+                         data=form_data_post, timeout=5)  # 第5次 post
+    page = lxml.html.document_fromstring(r.text)
 
-        else:
-            print('[INFO] No regular receiver!')
+    fee = float(page.xpath("//span[@class='number orange']/text()")[0])
 
-        lowfee_mailclient = [to_addr for [to_addr, isRegular] in mailclient.items() if not isRegular]
+    return fee
 
-        if lowfee_mailclient:
-            
-            if fee < fee_threshold:
-                print('[INFO] send mail to lowfee receiver' + str(lowfee_mailclient))
-                lowfeeMailSend(mailserver_username=mailserver_username,
-                               mailserver_password=mailserver_password,
-                               mailserver_smtp=mailserver_smtp,
-                               mailclient=lowfee_mailclient,
-                               clientroom=room,
-                               fee=fee)
-        else:
-            print('[INFO] No lowfee receiver!')
+
+def TEB_getInfo(TEB_jsonData):
+    '''
+    return: 
+    {'1_8_307': {
+        'mailclient': {
+            '******@qq.com': 0,
+            '******@qq.com': 1
+        },
+        'Fee': 3.39
+    }
+    }
+    '''
+    TEB_Info = {}
+    admin_MailMsg = []
+    url = TEB_jsonData['target_url']
+    headers = TEB_jsonData['headers']
+
+    # mailserver_username = TEB_jsonData['mailserver_username']  # 发送提醒邮件的邮箱
+    # mailserver_password = TEB_jsonData['mailserver_password']  # 发送提醒邮件邮箱的密码
+    # mailserver_smtp = TEB_jsonData['mailserver_smtp']  # 发送邮箱的SMPT服务器地址
+    # admin_mail = TEB_jsonData['admin_mailaddr']  # 管理员邮箱
+    subkey = ['mailserver_username', 'mailserver_password',
+              'mailserver_smtp', 'admin_mailaddr']
+    admin_MailInfo = {key: TEB_jsonData[key] for key in subkey}
+
+    for room in TEB_jsonData['clientroom']:
+        alarm_threshold = TEB_jsonData['clientroom'][room]['alarm_threshold']
+        form_data = TEB_jsonData['clientroom'][room]['form_data']
+        mailclient = TEB_jsonData['clientroom'][room]['client_mailaddr']
+        fee = TEB_getFeeFromPage(url, headers, form_data)
+        TEB_Info[room] = {'Fee': fee, 'mailclient': mailclient,
+                          'alarm_threshold': alarm_threshold}
+        roominfo = room.split('_')
+
+        msg = " 嘉定" + " 校区  " + \
+            roominfo[1] + " 号楼  " + roominfo[2] + " 房间 " + " 电费还剩：" + str(fee)
+        admin_MailMsg.append(msg)
+
+    return TEB_Info, admin_MailInfo, admin_MailMsg
+
+
+def TEB_Info_log():
+    '''
+
+    '''
+    localtime = time.localtime()
+    print(time.strftime('%Y-%m-%d %H:%M:%S', localtime))
+    return
+
+
+def send_adminmail(admin_MailInfo, admin_MailMsg):
+
+    return
+
+
+def main():
+    '''
+
+    '''
+    TEB_jsonData = TEB_readfromjson(mode=0)  # 从json文件中读取条目
+    TEB_Info,  admin_MailInfo, admin_MailMsg = TEB_getInfo(TEB_jsonData)
+
+    print(TEB_Info)
+    print(admin_MailInfo)
+    print(admin_MailMsg)
+
+    regular_info, lowfee_info = TEB_Info_to_mailsend_Info(TEB_Info)
+    print(regular_info)
+    print(lowfee_info)
+    mailSend(TEB_Info, admin_MailInfo)
+
 
 if __name__ == '__main__':
     main()
--- mailsend.py
+++ mailsend.py
@@ -12,20 +12,67 @@ def _format_addr(s):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 
-def regularMailSend(mailserver_username, mailserver_password, mailserver_smtp, mailclient, clientroom, fee):
+def TEB_Info_to_mailsend_Info(TEB_Info):
+    regular_info = []
+    lowfee_info = []
+    for room in TEB_Info:
+        mailclient = TEB_Info[room]['mailclient']
+        fee = TEB_Info[room]['Fee']
+        alarm_threshold = TEB_Info[room]['alarm_threshold']
 
-    from_addr = mailserver_username
-    password = mailserver_password
-    smtp_server = mailserver_smtp
-    to_addr = mailclient
+        regular_mailclient = [to_addr for [
+            to_addr, isRegular] in mailclient.items() if isRegular]
+        lowfee_mailclient = [to_addr for [to_addr, isRegular]
+                             in mailclient.items() if not isRegular]
+
+        for item in regular_mailclient:
+            regular_info.append([item, room, fee])
+        for item in lowfee_mailclient:
+            if fee < alarm_threshold:
+                lowfee_info.append([item, room, fee])
+
+    return regular_info, lowfee_info
+
+
+def mailSend(TEB_Info, admin_MailInfo):
+    regular_info, lowfee_info = TEB_Info_to_mailsend_Info(TEB_Info)
+    if regular_info:
+        print('[INFO] Send mail to regular receiver: ' + ' \n')
+        for regular_info_item in regular_info:
+            regularMailSend(regular_info_item, admin_MailInfo)
+    else:
+        print('[INFO] No regular receiver!\n')
+
+    if lowfee_info:
+        print('[INFO] send mail to lowfee receiver' + ' \n')
+        for lowfee_info_item in lowfee_info:
+            lowfeeMailSend(lowfee_info_item, admin_MailInfo)
+    else:
+        print('[INFO] No lowfee receiver!\n')
+
+    return
+
+
+def regularMailSend(mailsend_Info, admin_MailInfo):
+    '''
+
+    '''
+    from_addr = admin_MailInfo['mailserver_username']
+    password = admin_MailInfo['mailserver_password']
+    smtp_server = admin_MailInfo['mailserver_smtp']
+    to_addr = mailsend_Info[0]
     # to_addr应该是被逗号分割的字符串，不能是list
+    clientroom = mailsend_Info[1]
+    fee = mailsend_Info[2]
+    roominfo = clientroom.split('_')
 
     body = (" (つд⊂) \n"
-            + " 嘉定" + " 校区  " +
-            str(clientroom)[-4] + " 号楼  " + str(clientroom)[-3:] + " 房间 \n"
+            + " 嘉定" + " 校区  " + roominfo[1] + " 号楼  " + roominfo[2] + " 房间 \n"
             + " 电费还剩：" + str(fee))
-    msg_from = ",".join(map(lambda from_addr: formataddr((Header("勤奋的Robot", 'utf-8').encode(), from_addr)), [from_addr]))
-    msg_to = ",".join(map(lambda to_addr: formataddr((Header("慵懒的管理员", 'utf-8').encode(), to_addr)), to_addr))
+    msg_from = ",".join(map(lambda from_addr: formataddr(
+        (Header("勤奋的Robot", 'utf-8').encode(), from_addr)), [from_addr]))
+    msg_to = ",".join(map(lambda to_addr: formataddr(
+        (Header("慵懒的管理员", 'utf-8').encode(), to_addr)), to_addr))
     msg_subject = '[RPI_TEB]电费报告...'
 
     msg = MIMEText(body, 'plain', 'utf-8')
@@ -42,23 +89,26 @@ def regularMailSend(mailserver_username, mailserver_password, mailserver_smtp, m
         server.quit()
 
 
-def lowfeeMailSend(mailserver_username, mailserver_password, mailserver_smtp, mailclient, clientroom, fee):
+def lowfeeMailSend(mailsend_Info, admin_MailInfo):
 
-    from_addr = [mailserver_username]
-    password = mailserver_password
-    smtp_server = mailserver_smtp
-    to_addr = mailclient
+    from_addr = admin_MailInfo['mailserver_username']
+    password = admin_MailInfo['mailserver_password']
+    smtp_server = admin_MailInfo['mailserver_smtp']
+    to_addr = mailsend_Info[0]
     # to_addr应该是被逗号分割的字符串，不能是list
+    clientroom = mailsend_Info[1]
+    fee = mailsend_Info[2]
+    roominfo = clientroom.split('_')
 
     body = (" (つд⊂) \n"
-            + " 嘉定" + " 校区  " +
-            str(clientroom)[-4] + " 号楼  " + str(clientroom)[-3:] + " 房间 \n"
+            + " 嘉定" + " 校区  " + roominfo[1] + " 号楼  " + roominfo[2] + " 房间 \n"
             + " 电费还剩：" + str(fee))
     msg_from = ", ".join(map(lambda from_addr: formataddr(
-        (Header("勤奋的Robot", 'utf-8').encode(), from_addr)), from_addr))
+        (Header("勤奋的Robot", 'utf-8').encode(), from_addr)), [from_addr]))
     msg_to = ", ".join(map(lambda to_addr: formataddr(
         (Header("慵懒的管理员", 'utf-8').encode(), to_addr)), to_addr))
     msg_subject = '[RPI_TEB]又没电了(╯°Д°)╯︵ ┻━┻'
+    # msg_subject = '[RPI_TEB]电费报告...'
 
     msg = MIMEText(body, 'plain', 'utf-8')
     msg['From'] = msg_from
@@ -72,15 +122,3 @@ def lowfeeMailSend(mailserver_username, mailserver_password, mailserver_smtp, ma
         server.sendmail(from_addr, to_addr, msg.as_string())
     finally:
         server.quit()
-
-'''
-# %%
-from email.header import Header
-from email.utils import parseaddr, formataddr
-mailclient = ["ycg1024@qq.com", "470413803@qq.com", "470413809@qq.com"]
-#mailclient = ["ycg1024@qq.com"]
-# mailclient = map(lambda x: "慵懒的管理员 <" + x + ">", mailclient)
-to_addr = ", ".join(map(lambda addr: formataddr(
-    (Header("慵懒的管理员", 'utf-8').encode(), addr)), mailclient))
-print(to_addr)
-'''
